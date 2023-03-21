5c808b557604b5cf5ebc1cd5e4e8ee0dcbc8cadd
Eric Neves <ericnevesrodrigues@gmail.com>
2023-03-20 22:01:18 -0400

    Modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./styles/main.css"
+import "./styles/content-map.css"
 import "./styles/content-form.css"
 import "./styles/content-info.css"
-import "./styles/content-map.css"
+
 
 import "./utils/renderMap"
 import "./App"
