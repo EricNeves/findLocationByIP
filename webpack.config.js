ca70ddb66c57ae7530803ea674e9b8a597c53893
Eric Neves <ericnevesrodrigues@gmail.com>
2023-03-20 21:46:04 -0400

    Add postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
             }, 
             {
                 test: /\.js$/,
